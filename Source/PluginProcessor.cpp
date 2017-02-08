0119f1473bfe51250607c006af90b8eca5aa7b0a
smacla200 <smacla200@caledonian.ac.uk>
2017-02-08 13:58:45 +0000

    change pan code

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -123,27 +123,26 @@ bool StereoPannerAudioProcessor::isBusesLayoutSupported (const BusesLayout& layo
 
 void StereoPannerAudioProcessor::processBlock (AudioSampleBuffer& buffer, MidiBuffer& midiMessages)
 {
-    const int totalNumInputChannels  = getTotalNumInputChannels();
-    const int totalNumOutputChannels = getTotalNumOutputChannels();
-
-    // In case we have more outputs than inputs, this code clears any output
-    // channels that didn't contain input data, (because these aren't
+    // In case we have more outputs than inputs, this code clears any output // channels that didn't contain input data, (because these aren't
     // guaranteed to be empty - they may contain garbage).
-    // This is here to avoid people getting screaming feedback
-    // when they first compile a plugin, but obviously you don't need to keep
-    // this code if your algorithm always overwrites all the output channels.
-    for (int i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
-        buffer.clear (i, 0, buffer.getNumSamples());
-
-    // This is the place where you'd normally do the guts of your plugin's
-    // audio processing...
-    for (int channel = 0; channel < totalNumInputChannels; ++channel)
-    {
-        float* channelData = buffer.getWritePointer (channel);
-
-        // ..do something to the data...
-    }
+    // I've added this to avoid people getting screaming feedback
+    // when they first compile the plugin, but obviously you don't need to // this code if your algorithm already fills all the output channels.
+    for (int i = getNumInputChannels(); i < getNumOutputChannels(); ++i)
+    buffer.clear (i, 0, buffer.getNumSamples());
+    // Retrieve the total number of samples in the buffer for this block
+    int numSamples = buffer.getNumSamples();
+    // channelDataL and channelDataR are pointers to arrays of length numSamples which // contain the audio for one channel. You repeat this for each channel
+    float *channelDataL = buffer.getWritePointer(0);
+    float *channelDataR = buffer.getWritePointer(1);
+    // calculate p’
+    float pDash = 3.14159*(panPosition + 1.0) / 4.0;
+    // Loop runs from 0 to number of samples in the block
+    for (int i = 0; i < numSamples; ++i) {
+        // Simple linear panning algorithm where:
+        channelDataL[i] = channelDataL[i] * (1.0 - pDash);
+        channelDataR[i] = channelDataR[i] * pDash; }
 }
+
 
 //==============================================================================
 bool StereoPannerAudioProcessor::hasEditor() const
